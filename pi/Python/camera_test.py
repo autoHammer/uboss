edb6472757abd429ba6b6fd82a85b3a48fd9deb2
SiggoMode <93973763+SiggoMode@users.noreply.github.com>
2024-03-15 16:09:27 +0100

    Added camera test + TCP sending

--- pi/Python/camera_test.py
+++ pi/Python/camera_test.py
@@ -1,12 +1,48 @@
+from picamera2 import Picamera2
+import socket
 import cv2
+import pickle
+import struct
 
-cap = cv2.VideoCapture(0)
+camera = Picamera2()
 
-while True:
-    ret, frame = cap.read()
+# Configure the camera
+preview_config = camera.create_preview_configuration()
+camera.configure(preview_config)
+camera.start()
+print("Test")
 
-    # Add code to display the frame and detections...
-    cv2.imshow('YOLOv7 Detections', frame)
-    if cv2.waitKey(1) == ord('q'):  # Exit loop if 'q' is pressed
-        break
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('10.24.90.17', 8485))
 
+img_counter = 0
+
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+try:
+    while True:
+        frame = camera.capture_array()
+
+        result, image = cv2.imencode('.jpg', frame)
+        data = pickle.dumps(image, 0)
+        size = len(data)
+
+        client_socket.sendall(struct.pack(">L", size) + data)
+
+        img_counter += 1
+
+
+except KeyboardInterrupt:
+    print("\nInterrupted")
+    print("Connection lost")
+    client_socket.close()
+    camera.stop()
+
+
+except:
+    print("\nCrash")
+    print("Connection lost...")
+    client_socket.close()
+    camera.stop()

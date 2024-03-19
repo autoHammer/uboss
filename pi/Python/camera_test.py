6fdda3301a79555ec5c1a48aef60239b82243108
SiggoMode <93973763+SiggoMode@users.noreply.github.com>
2024-03-19 14:27:01 +0100

    minor changes

--- pi/Python/camera_test.py
+++ pi/Python/camera_test.py
@@ -1,8 +1,11 @@
 from picamera2 import Picamera2
+from ultralytics import YOLO
 import socket
 import cv2
 import pickle
 import struct
+import os
+import numpy as np
 
 camera = Picamera2()
 
@@ -10,29 +13,46 @@ camera = Picamera2()
 preview_config = camera.create_preview_configuration()
 camera.configure(preview_config)
 camera.start()
-print("Test")
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+
+# Model configuration:
+os.chdir("..")
+IMAGE_RECOGNITION_DIR = os.getcwd() + "/Image_Recognition"
+
+YOLOV8_PT_S = IMAGE_RECOGNITION_DIR + "/yolov8/Weights/best_s.pt"
+
+model = YOLO(YOLOV8_PT_S)
+
 
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('10.24.90.17', 8485))
+client_socket.connect(('10.24.45.49', 8485))
 
-img_counter = 0
-
-
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 try:
     while True:
         frame = camera.capture_array()
 
+        cvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = model.predict(cvframe)
+
+        for box in results[0].boxes:
+            x = int(np.floor(box[0].xywh[0][0].item()))
+            y = int(np.floor(box[0].xywh[0][1].item()))
+            w = int(np.floor(box[0].xywh[0][2].item()))
+            h = int(np.floor(box[0].xywh[0][3].item()))
+
+            cv2.rectangle(cvframe, (x, y), (x + w, y + h), (0, 255, 0))
+
+        # Fungerende gammel tcp kode
+        """
         result, image = cv2.imencode('.jpg', frame)
         data = pickle.dumps(image, 0)
         size = len(data)
 
         client_socket.sendall(struct.pack(">L", size) + data)
-
-        img_counter += 1
-
+        """
 
 except KeyboardInterrupt:
     print("\nInterrupted")
@@ -41,8 +61,14 @@ except KeyboardInterrupt:
     camera.stop()
 
 
-except:
+except socket.error:
     print("\nCrash")
     print("Connection lost...")
     client_socket.close()
     camera.stop()
+
+
+finally:
+    client_socket.close()
+    camera.stop()
+

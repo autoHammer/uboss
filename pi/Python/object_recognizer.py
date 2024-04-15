78c4b6e3de9ca00782bc8243b4b5a437939fca39
SiggoMode <93973763+SiggoMode@users.noreply.github.com>
2024-04-15 15:23:59 +0200

    Threaded prediction boxes to QGC

---
+++ pi/Python/object_recognizer.py
@@ -0,0 +1,76 @@
+import threading
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import time
+from Other.thread_safe_value import ThreadSafeValue
+
+# Recognition font settings
+# Recognition font settings:
+font = cv2.FONT_HERSHEY_SIMPLEX  # Font type
+font_scale = 1  # Font size
+font_color = (255, 255, 255)  # Font color (white in this case)
+font_thickness = 1  # Thickness of the text
+
+
+class ObjectDetector:
+    def __init__(self, model):
+        print("Current dir: ", model)
+        self._model = YOLO(model)
+        self._results = ThreadSafeValue()
+        self._current_frame = None
+        self._detector_thread = None
+
+    def detect(self, frame):
+        # Update frame
+        self._current_frame = frame
+
+        # Initial run:
+        if self._detector_thread is None:
+            self._detector_thread = threading.Thread(target=self.detector_thread, args=(self._current_frame,))
+            self._detector_thread.start()
+
+        elif not self._detector_thread.is_alive():
+            self._detector_thread = threading.Thread(target=self.detector_thread, args=(self._current_frame,))
+            self._detector_thread.start()
+
+
+    def detector_thread(self, frame):
+        results = self._model.predict(frame)
+        self._results.set(results)
+
+    def draw_boxes(self, frame):
+        if (self._current_frame is None) or (self._results.take() is None):
+            return frame
+
+        else:
+            results = self._results.take()
+            for box in results[0].boxes:
+                # Drawing boxes
+                x = int(np.floor(box[0].xywh[0][0].item()))
+                y = int(np.floor(box[0].xywh[0][1].item()))
+                w = int(np.floor(box[0].xywh[0][2].item()))
+                h = int(np.floor(box[0].xywh[0][3].item()))
+                cv2.rectangle(self._current_frame, (x - w // 2, y - h // 2), (x + w // 2, y + h // 2), (0, 140, 255))
+
+                # Writing object names:
+                c = box.cls
+                detected_object = self._model.names[int(c)]
+
+                # Calculate text size to position it correctly
+                text_size = cv2.getTextSize(detected_object, font, font_scale, font_thickness)[0]
+                text_x = x - w // 2
+                text_y = y - h // 2 - 10  # Position the text 10px above the rectangle
+
+                # Ensure the text background rectangle does not go out of the image bounds
+                background_top_left = (text_x, text_y - text_size[1] - 2)
+                background_bottom_right = (text_x + text_size[0], text_y + 2)
+                cv2.rectangle(self._current_frame, background_top_left, background_bottom_right, (0, 140, 255),
+                              cv2.FILLED)
+
+                # Put text
+                cv2.putText(self._current_frame, detected_object, (text_x, text_y), font, font_scale, font_color,
+                            font_thickness)
+
+            return self._current_frame
+
--- pi/Python/usb_camera_test.py
+++ pi/Python/usb_camera_test.py
@@ -2,27 +2,22 @@ import gi
 import cv2
 import numpy as np
 from ultralytics import YOLO
+from gi.repository import Gst, GLib
 import os
+from object_recognizer import ObjectDetector
 
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GLib
 
 # Initialize GStreamer
 Gst.init(None)
 
 # Parameters:
-IMAGE_RECOGNITION_DIR = os.getcwd() + "/ImageRecognitionPTs"
+IMAGE_RECOGNITION_DIR = os.getcwd() + "/../Image_Recognition"
 
-YOLOV8_PT_S = IMAGE_RECOGNITION_DIR + "/yolov8/v8_small_first_best.pt"
+YOLOV8_PT_S = IMAGE_RECOGNITION_DIR + "/yolov8/Weights/v8_small_first_best.pt"
 
-model = YOLO(YOLOV8_PT_S)
-
-# Recognition font settings:
-# Text settings
-font = cv2.FONT_HERSHEY_SIMPLEX  # Font type
-font_scale = 1  # Font size
-font_color = (255, 255, 255)  # Font color (white in this case)
-font_thickness = 1  # Thickness of the text
+# Detector object
+object_detector = ObjectDetector(YOLOV8_PT_S)
 
 
 class VideoProcessor:
@@ -61,15 +56,8 @@ class VideoProcessor:
                 dtype=np.uint8)
 
             # Image processing:
-            if self.counter == 0:
-                self.recognition_results = model.predict(frame)
-
-            self.counter += 1
-            if self.counter == self._RECOGNITION_INTERVAL:
-                self.counter = 0
-
-            # Draw boxes and text
-            frame = self.draw_boxes(frame)
+            object_detector.detect(frame)
+            frame = object_detector.draw_boxes(frame)
 
             self.push_frame_to_appsrc(frame)
 
@@ -89,34 +77,6 @@ class VideoProcessor:
     def stop(self):
         self.capture_pipeline.set_state(Gst.State.NULL)
         self.stream_pipeline.set_state(Gst.State.NULL)
-
-    def draw_boxes(self, frame):
-        for box in self.recognition_results[0].boxes:
-            # Drawing boxes
-            x = int(np.floor(box[0].xywh[0][0].item()))
-            y = int(np.floor(box[0].xywh[0][1].item()))
-            w = int(np.floor(box[0].xywh[0][2].item()))
-            h = int(np.floor(box[0].xywh[0][3].item()))
-            cv2.rectangle(frame, (x - w // 2, y - h // 2), (x + w // 2, y + h // 2), (0, 140, 255))
-
-            # Writing object names:
-            c = box.cls
-            detected_object = model.names[int(c)]
-
-            # Calculate text size to position it correctly
-            text_size = cv2.getTextSize(detected_object, font, font_scale, font_thickness)[0]
-            text_x = x - w // 2
-            text_y = y - h // 2 - 10  # Position the text 10px above the rectangle
-
-            # Ensure the text background rectangle does not go out of the image bounds
-            background_top_left = (text_x, text_y - text_size[1] - 2)
-            background_bottom_right = (text_x + text_size[0], text_y + 2)
-            cv2.rectangle(frame, background_top_left, background_bottom_right, (0, 140, 255), cv2.FILLED)
-
-            # Put text
-            cv2.putText(frame, detected_object, (text_x, text_y), font, font_scale, font_color, font_thickness)
-
-            return frame
 
 
 def main():

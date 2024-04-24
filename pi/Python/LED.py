3da00e7d3f545719d333f2d656e0cd6e6893ea55
William <60541285+autoHammer@users.noreply.github.com>
2024-04-24 13:54:05 +0200

    cleanup

--- pi/Python/LED.py
+++ pi/Python/LED.py
@@ -7,13 +7,14 @@ def map(min_in, max_in, min_out, max_out, value):
     return shifted_value
 
 
-camera = PWMLED(pin=21, frequency=150)
+camera = PWMLED(pin=21, frequency=50)
 
 print("Welcome to light control center. Press Ctrl+C to exit.")
 
 while True:
     light_level = float(input("Write light percentage (0-100): "))
-    duty_cycle = map(0, 100, 0.17, 0.27, light_level)
+    duty_cycle = map(0, 100, 0.058, 0.091, light_level)
     camera.value = duty_cycle
+    print("duty cycle: ", duty_cycle)
     sleep(0.5)
 
